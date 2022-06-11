a2ecea1ca0001612ee3f25b75d05074367b00c4c
tygrski <77369211+tygrski@users.noreply.github.com>
2022-06-11 13:49:39 -0500

    end of lesson 3

---
+++ js/youtube.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function () {
+  document.getElementById("confirm").innerHTML = "order placed check email for confirmation";
+  document.getElementById("button").style.display = "none";
+};
