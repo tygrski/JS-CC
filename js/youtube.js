d12d2d5636dd64bc7023b561cf136ae5bf996369
tygrski <77369211+tygrski@users.noreply.github.com>
2022-06-11 20:17:03 -0500

    block vs global scope

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,20 @@
-document.getElementById("button").onclick = function () {
-  document.getElementById("confirm").innerHTML = "order placed check email for confirmation";
-  document.getElementById("button").style.display = "none";
-};
+(function(){
+//  IFFE - immediately invoked funciton expression
+var age = 5;
+
+
+
+})();
+
+{
+    // block
+    let y = 10;
+    let x = 15;
+    {
+        let y = 6;
+        console.log(y);
+    }
+    console.log(y);
+    console.log(x,y);
+    var z = 100;
+}
