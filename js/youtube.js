74e54f69f89191eb22c2660337899318bd789ff8
tygrski <77369211+tygrski@users.noreply.github.com>
2022-06-14 18:35:23 -0500

    string

--- js/youtube.js
+++ js/youtube.js
@@ -1,20 +1,27 @@
-(function(){
-//  IFFE - immediately invoked funciton expression
-var age = 5;
+// {
+//     let myName = "Travis";
+//     console.log(`My name is ${myName}`);
+   
+// }
 
-
-
-})();
+// {
+//     let test = " lorem ipsum\
+//      bla\
+//      blahh"
+//      console.log(test.length);
+// }
 
 {
-    // block
-    let y = 10;
-    let x = 15;
-    {
-        let y = 6;
-        console.log(y);
-    }
-    console.log(y);
-    console.log(x,y);
-    var z = 100;
+let content = "     TODAY will we be talking about strings"
+let search1 = "string";
+
+console.log(content.substring(6, 20));
+console.log(content.substr(6,20));
+console.log(content.slice(6,20));
+console.log(content.toUpperCase());
+
+console.log(content.trim());
+console.log(content.trimLeft());
+console.log(content.split(""));
 }
+
