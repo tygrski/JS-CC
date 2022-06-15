a9b501a847e0d12275ef0520a9b496213264ba12
tygrski <77369211+tygrski@users.noreply.github.com>
2022-06-14 21:33:07 -0500

    ternanry operators

--- js/youtube.js
+++ js/youtube.js
@@ -1,22 +1,90 @@
-// functions and objects
-{
-    let position = {
-        x: 10,
-        y: 20,
-        print: function(){
-            console.log( `x: ${this.x}, y: ${this.y}`);
-        }
-         
-    }
+// if statements
+// {
 
-    let myPosition = position;
-    
-    console.log(position);
-    console.log(myPosition);
+//     var name = prompt();
+//     if(name === "travis"){  // identity operator // strict eqiality
+//         console.log("welcome");   
+//      }else if (name === "tom"){
+//         console.log("hi tom")
+//      }
+//      else{
+//         console.log("you are not welcome")
+//      }
+// }
 
-    myPosition.x = 15;
+// compare operators 
+// {
 
-    console.log(position);
-    console.log(myPosition);
-    position.print();
-}
+//     var age = prompt();
+//     if(age < 12){  // identity operator // strict eqiality
+//         console.log("welcome");   
+//      }
+     
+// }
+
+
+// logical operators
+// {
+
+//     var age = prompt();
+//     if(age < 13 || age >= 65 ){ 
+//         console.log("welcome");   
+//      }
+     
+// }
+
+// {
+
+//     var age = prompt('AGE');
+//     var name = prompt('Name');
+
+//     if(age < 12 && name === "travis" ){  
+//         console.log("welcome");   
+//      }
+     
+// }
+
+//  swith statement
+// {
+//     let name =prompt("what is your name");
+
+//     switch(name)
+//     {
+//         case "travis":
+//             console.log('welcome');
+//             break;
+//         case "Tom":
+//             console.log("bye");
+//             break;
+//         default:
+//             console.log("you can enter");
+//             break;
+//     }
+// }
+
+// single line if statement
+// {
+// let name = prompt("what is your name?");
+// if(name === "travis") console.log("hi")
+
+
+// console.log("this will print each time beacuse above is attached to if not this");
+// }
+
+// ================
+// ternary operator
+
+// {
+//     let name = prompt("what is my name?");
+//     let points = name === "travis" ? 10 : 0;
+//     console.log(points);
+// }
+
+
+// {
+//     let name = prompt("what is my name?");
+//     name === "travis" ? console.log(10) : console.log(0);
+//     ;
+// }
+
+// into to loops
