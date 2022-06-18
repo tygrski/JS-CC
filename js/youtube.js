5bddbf1da83c0a0b35908928226e65fb831b46af
tygrski <77369211+tygrski@users.noreply.github.com>
2022-06-17 21:11:03 -0500

    loops

--- js/youtube.js
+++ js/youtube.js
@@ -3,7 +3,7 @@
 
 //     var name = prompt();
 //     if(name === "travis"){  // identity operator // strict eqiality
-//         console.log("welcome");   
+//         console.log("welcome");
 //      }else if (name === "tom"){
 //         console.log("hi tom")
 //      }
@@ -12,25 +12,24 @@
 //      }
 // }
 
-// compare operators 
+// compare operators
 // {
 
 //     var age = prompt();
 //     if(age < 12){  // identity operator // strict eqiality
-//         console.log("welcome");   
+//         console.log("welcome");
 //      }
-     
-// }
 
+// }
 
 // logical operators
 // {
 
 //     var age = prompt();
-//     if(age < 13 || age >= 65 ){ 
-//         console.log("welcome");   
+//     if(age < 13 || age >= 65 ){
+//         console.log("welcome");
 //      }
-     
+
 // }
 
 // {
@@ -38,10 +37,10 @@
 //     var age = prompt('AGE');
 //     var name = prompt('Name');
 
-//     if(age < 12 && name === "travis" ){  
-//         console.log("welcome");   
+//     if(age < 12 && name === "travis" ){
+//         console.log("welcome");
 //      }
-     
+
 // }
 
 //  swith statement
@@ -67,7 +66,6 @@
 // let name = prompt("what is your name?");
 // if(name === "travis") console.log("hi")
 
-
 // console.log("this will print each time beacuse above is attached to if not this");
 // }
 
@@ -80,7 +78,6 @@
 //     console.log(points);
 // }
 
-
 // {
 //     let name = prompt("what is my name?");
 //     name === "travis" ? console.log(10) : console.log(0);
@@ -88,3 +85,95 @@
 // }
 
 // into to loops
+
+//  while loop
+// {
+//   let i = 1;
+//   while (i < 11) {
+//     console.log(i);
+//     i++;
+//   }
+// }
+
+// ===========
+// do while
+// {
+//     let i = 0;
+//     do{
+//         console.log(i);
+//         i++
+//     } while( i < 12);
+// }
+// =================
+// for  loop
+// {
+// for ( i = 0; i < 20; i++) {
+//  console.log(i);
+// }
+// }
+
+// {
+//     let password;
+//     do{
+//         password = prompt("what is the password?");
+//     }while(password.toLocaleLowerCase() !== "let me in");
+// }
+
+// for loop
+// {
+
+//     let list = [1,2,3,4,5,6,8,9]
+// for ( let i = 0; i <  list.length ; i++ ){
+//     console.log(list[i]);
+// }
+// }
+
+// ============================
+// same as above with search and continue
+// {
+//   let myString = "search this string baby";
+//   let charToSearch = "c";
+//   for (let i = 0; i < myString.length; i++) {
+//     if (myString[i] === charToSearch) {
+//       console.log(myString[i] + "is found at index of " + i );
+//     }
+//   }
+// }
+
+// ============
+// same as above with search and continue
+// {
+//     let myString = "search this string baby cc and c";
+//     let charToSearch = "c";
+//     for (let i = 0; i < myString.length; i++) {
+//       if (myString[i] === charToSearch) {
+//         console.log(myString[i] + " is found at index of " + i );
+//         break;
+//       }
+//     }
+//   }
+
+//========== with continue
+// filtering out the vowels in myString
+// {
+//   let myString = "search this string baby cc and ccc";
+//   for (let i = 0; i < myString.length; i++) {
+//     if (myString[i] === "a" ||  myString[i] === 'e' || myString[i] === 'i'){
+//       continue;
+//     }
+//     console.log(myString[i]);
+//   }
+// }
+
+// ===========================================
+// nested loops
+{
+    let d = document.getElementById("destination");
+  for (let i = 0; i < 10; i++) {
+    for( let k = i; k >= 0; k--){
+        d.append(k + " ");
+    }
+    var br = document.createElement('br');
+    d.appendChild(br);
+  }
+}
