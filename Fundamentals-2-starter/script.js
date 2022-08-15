6c87ad5729462758703daa471bfc63f583455959
tygrski <77369211+tygrski@users.noreply.github.com>
2022-08-15 16:19:34 -0500

    arrays and challenge 2

--- Fundamentals-2-starter/script.js
+++ Fundamentals-2-starter/script.js
@@ -96,7 +96,7 @@ function fruitProcessor(apples, oranges){
     return juice;  
 }
 console.log(fruitProcessor(2, 3));
-*/
+
 
 //  Review Func
 
@@ -150,4 +150,111 @@ scoreDolphin = calcAverage(85, 54, 41);
 scoreKoala = calcAverage(23, 34, 27);
 console.log(scoreDolphin, scoreKoala);
 checkWinner(scoreDolphin, scoreKoala);
+
+
+// Arrays
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+//  Array function, parenthesis and key word new Array
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(new Array());
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+//  cant do this
+// friends = ["Bob"];
+
+const firstName = "Travis";
+const travis = [firstName, "Tybor", 2022 - 1980, friends];
+console.log(travis);
+console.log(travis.length);
+
+// array exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1980, 1967, 2022, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
+//  Basic ARRAY Methods
+
+const friends = ["Michael", "Steven", "Peter"];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+//frineds.push('Jay') returns the value of the array lenght
+console.log(newLength);// logs 4
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // last value, doesn't need a arguement, sinve it removes last element. 
+// pop returns a value , not the lenght, but the element it removed
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+friends.shift();// First
+console.log(friends)
+
+// position of element
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob')); // -1 returns a value , bob is not in array, -1
+//boolean true or false value returned
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+
+friends.push(23);
+// below wont work bec it's Strng
+console.log(friends.includes('23'));
+//will work 
+console.log(friends.includes(23));
+console.log(friends);
+
+if(friends.includes('Steven')){
+    console.log('you have a friend named Steven');
+}
+
+// Sec 2 - Challenge 2
+
+const calcTip = function(bill) {
+     return bill >= 50 && bill <= 300 ? bill *0.25 : bill * .2
+}
+// Arrow version
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.25 : bill * 0.2;
+
+const bill = [125, 555, 44];
+const tip = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
+
+console.log(bill, tip);
+
+
+const billMeal = [125, 555, 44];
+const tip1 = calcTip(bill[0]);
+const tip2 = calcTip(bill[1]);
+const tip3 = calcTip(bill[2]);
+
+
+console.log(tip1, tip2, tip3);
+
+//  Bonus 
+const total = [bill[0] + tip[0], bill[1] + tip[1], bill[2] + tip[2]]
+console.log(total);
+
 
