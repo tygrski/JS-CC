5ab21b1ab4b3401539f0f8d598266f55926cdd35
tygrski <77369211+tygrski@users.noreply.github.com>
2022-09-02 14:05:04 -0500

    hoisting

--- -js-behind-the -scenes/script.js
+++ -js-behind-the -scenes/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/* 
 function calcAge(birthYear){
     const age  = 2037 - birthYear;
     
@@ -33,3 +34,54 @@ function calcAge(birthYear){
 }
 const firstName ='Travis';
 calcAge(1990);
+
+*/
+
+// Hoisting
+
+//  with variables
+console.log(me);
+// console.log(job);
+// console.log(birthYear);
+
+var me = 'Travis';
+let job = 'student';
+const birthYear = 1980;
+console.log(me);
+
+// hoistin with Functions
+console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+//  functions with function keyword are hoisted
+ function addDecl(a,b){
+    return a + b;
+ }
+
+ const addExpr = function(a,b) {
+    return a + b;
+ }
+
+ const addArrow = (a,b) => a +b;
+
+//  Example
+
+if(!numProducts) deleteShoppingCart();
+
+// code above will run , but value is undefined or !numproducts, so cl will work
+var numProducts = 10;
+
+function deleteShoppingCart (){
+    console.log(`all products deleted`);
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+// check to see if part of window object
+// only var is part of the window object
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
