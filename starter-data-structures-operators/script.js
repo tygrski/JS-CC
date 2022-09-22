dc5198b5966dcecdf2593791984dcec5955724de
tygrski <77369211+tygrski@users.noreply.github.com>
2022-09-22 16:02:06 -0500

    desrtucturing arrays

---
+++ starter-data-structures-operators/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+}
+};
+  const arr =[2,3,4];
+  const a = arr[0]
+  const b = arr[1]
+  const c = arr[2]
+
+  // destructeured assignment
+  const [x, y, z] = arr; 
+  console.log(x,y,z);
+  console.log(arr);
+
+  //  ,  , make a whole to skip a number of the array
+  const [first, ,second] = restaurant.categories
+  console.log(first, second);
+
+  let [main, ,secondary] = restaurant.categories
+  console.log(main, secondary);
+
+
+  //  SWITICHING VARIABLES
+  // const temp = main;
+  // main = secondary;
+  // secondary = temp;
+  // console.log(main, secondary);
+
+ [main, secondary] = [secondary, main];
+ console.log(main, secondary);
+
+
+//  Recieve 2 Return Values from a Function
+console.log(restaurant.order(2, 0));
+const [starter, mainCourse] =  restaurant.order(2, 0)   
+console.log(starter, mainCourse);
+  
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+// console.log(i ,j);
+//  Nested destructuring
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//  Derfult Values 
+ const [p = 1, q = 1, r = 1] = [8, 9] 
+
+console.log(p, q, r);
+
+  //  switch categories
+
+  
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
+
