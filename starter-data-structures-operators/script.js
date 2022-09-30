36264e20f1e70594d7f58c978e0af35cd427cbf9
tygrski <77369211+tygrski@users.noreply.github.com>
2022-09-30 16:26:24 -0500

    for of loops

--- starter-data-structures-operators/script.js
+++ starter-data-structures-operators/script.js
@@ -1,89 +1,90 @@
 'use strict';
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-  [
-  'Neuer',
-  'Pavard',
-  'Martinez',
-  'Alaba',
-  'Davies',
-  'Kimmich',
-  'Goretzka',
-  'Coman',
-  'Muller',
-  'Gnarby',
-  'Lewandowski',
-  ],
-  [
-  'Burki',
-  'Schulz',
-  'Hummels',
-  'Akanji',
-  'Hakimi',
-  'Weigl',
-  'Witsel',
-  'Hazard',
-  'Brandt',
-  'Sancho',
-  'Gotze',
-  ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-  'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-  team1: 1.33,
-  x: 3.25,
-  team2: 6.5,
-  },
-  };
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//   [
+//   'Neuer',
+//   'Pavard',
+//   'Martinez',
+//   'Alaba',
+//   'Davies',
+//   'Kimmich',
+//   'Goretzka',
+//   'Coman',
+//   'Muller',
+//   'Gnarby',
+//   'Lewandowski',
+//   ],
+//   [
+//   'Burki',
+//   'Schulz',
+//   'Hummels',
+//   'Akanji',
+//   'Hakimi',
+//   'Weigl',
+//   'Witsel',
+//   'Hazard',
+//   'Brandt',
+//   'Sancho',
+//   'Gotze',
+//   ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+//   'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//   team1: 1.33,
+//   x: 3.25,
+//   team2: 6.5,
+//   },
+//   };
 
-// 1 Create one player array for each team (
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// // 1 Create one player array for each team (
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-// 2 The first player in any player array is the goalkeeper and the others are field 
-// players
-  const [gk, ...fieldPlayers] =  players1;
-  console.log(gk, fieldPlayers);
+// // 2 The first player in any player array is the goalkeeper and the others are field 
+// // players
+//   const [gk, ...fieldPlayers] =  players1;
+//   console.log(gk, fieldPlayers);
 
-// 3. Create an array 'allPlayers' containing all players of both teams
-const allPlayers = [...players1, ...players2]
-console.log(allPlayers);
+// // 3. Create an array 'allPlayers' containing all players of both teams
+// const allPlayers = [...players1, ...players2]
+// console.log(allPlayers);
 
-/* 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
-new array ('players1Final') containing all the original team1 players plus 
- 'Thiago', 'Coutinho' and 'Perisic' */
+// /* 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
+// new array ('players1Final') containing all the original team1 players plus 
+//  'Thiago', 'Coutinho' and 'Perisic' */
 
- const playersFinal = [ ...players1,'Thiago','Coutinho', 'Perisic' ];
- console.log(playersFinal);
+//  const playersFinal = [ ...players1,'Thiago','Coutinho', 'Perisic' ];
+//  console.log(playersFinal);
 
-// 5. Based on the game.odds object, create one variable for each odd (called 
-// 'team1', 'draw' and 'team2'
-const {odds: {team1, x: draw ,team2}} = game;
-console.log(team1, draw, team2);
+// // 5. Based on the game.odds object, create one variable for each odd (called 
+// // 'team1', 'draw' and 'team2'
+// const {odds: {team1, x: draw ,team2}} = game;
+// console.log(team1, draw, team2);
 
-// 6. Write a function ('printGoals') that receives an arbitrary number of player 
-// names (not an array) and prints each of them to the console, along with the 
-// number of goals that were scored in total (number of player names passed in)
+// // 6. Write a function ('printGoals') that receives an arbitrary number of player 
+// // names (not an array) and prints each of them to the console, along with the 
+// // number of goals that were scored in total (number of player names passed in)
 
-const printGoals = function(...players){
-  console.log(players);
-  console.log(`${players.length} goals scored`);
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals('Davies', 'Muller');
-printGoals(...game.scored);
+// const printGoals = function(...players){
+//   console.log(players);
+//   console.log(`${players.length} goals scored`);
+// };
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+// printGoals(...game.scored);
 
-/* 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary 
-operator*/
+// /* 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary 
+// operator*/
 
-team1 < team2 && console.log('team1 is more likely to win');
-team1 > team2 && console.log('team2 is more likely to win');
+// team1 < team2 && console.log('team1 is more likely to win');
+// team1 > team2 && console.log('team2 is more likely to win');
+
 
 // ===========================================
 
@@ -136,6 +137,24 @@ const restaurant = {
   }
 };
 
+// Looping Arrays  ===================================================
+// For Of Loop
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for ( const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+};
+
+console.log([...menu.entries()]);
+
+// Enhanced Object Literals ===============================
+
+
+
+/*
 // ========================================================
 // LOGICAL ASSIGNMENT OPERATORS
 const rest1 = {
@@ -183,6 +202,8 @@ console.log(guests);
 //  Nullish values are null or undefined, not 0 or ''
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
+*/
+
 
 /*
 ==============================================================
