da07e00a53c02b7aa12ef21c1751e6fd0dcb35c0
tygrski <77369211+tygrski@users.noreply.github.com>
2022-10-03 19:19:42 -0500

    optional chaining

--- starter-data-structures-operators/script.js
+++ starter-data-structures-operators/script.js
@@ -92,6 +92,25 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekDays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun' ];
+
+const  openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekDays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -99,27 +118,19 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-      thu: {
-        open: 12,
-        close: 22,
-      },
-      fri: {
-        open: 11,
-        close: 23,
-      },
-      sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-      },
-    },
 
-  order: function(starterIndex, mainIndex){
+  // ES6 enhanced opject literal
+  openingHours,
+ 
+
+  order(starterIndex, mainIndex){
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
 },
 
+
+
 // starterIndex, main and time have defaults added to params, incase there is none int he function call. It returns the default value
-  orderDelivery: function({
+  orderDelivery({
     starterIndex = 1, 
     mainIndex = 0, 
     time = '20:00', 
@@ -128,16 +139,49 @@ const restaurant = {
     console.log(`Order recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} and will be delivered to ${address} at ${time} `);
 },
 
-  orderPasta: function (ing1, ing2, ing3){
+  orderPasta(ing1, ing2, ing3){
     console.log(`Here is yoour pasta order with ${ing1}, ${ing2}, and ${ing3}`);
   },
-  orderPizza: function(mainIngredient, ...otherIngredients){
+  orderPizza(mainIngredient, ...otherIngredients){
     console.log(mainIngredient);
     console.log(otherIngredients);
   }
 };
 
-// Looping Arrays  ===================================================
+//==== Optional Chaining========================
+if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// With Optional Chaining. after ? will be checked if everything before ? exists 
+console.log(restaurant.openingHours.mon?.open);
+
+console.log(restaurant.openingHours?.mon?.open);
+
+//  Example
+const days = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun' ];
+
+for(const day of days){
+  // console.log(day);
+ const open = restaurant.openingHours[day]?.open ??  'closed';
+  console.log(`on ${day} we open at ${open}`);
+};
+
+//  Methods
+console.log(restaurant.order?.(0,1) ?? 'MEHTOD DOES NOT EXIST');
+console.log(restaurant.orderRisotto?.(0,1) ?? 'MEHTOD DOES NOT EXIST');
+
+//  Arrays
+
+const users = [
+  {
+    name: 'Traivs', 
+    email: 'hi@gmail.com',
+  }
+];
+
+console.log(users[0]?.name ?? 'User array epty');
+
+/*
+//=== Looping Arrays  ==============================
 // For Of Loop
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
@@ -149,13 +193,13 @@ for ( const [i, el] of menu.entries()) {
 };
 
 console.log([...menu.entries()]);
-
-// Enhanced Object Literals ===============================
+*/
+// Enhanced Object Literals ====================
 
 
 
 /*
-// ========================================================
+// ============================================
 // LOGICAL ASSIGNMENT OPERATORS
 const rest1 = {
   name: 'Capri',
@@ -206,7 +250,7 @@ console.log(guestsCorrect);
 
 
 /*
-==============================================================
+==================================================
 //  ShortCut && and ||
 
 //  Can use any data type, return any data type, short circuting 
