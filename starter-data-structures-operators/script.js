2d026fa3075b23426ba17b77cccf156a0578e4e8
tygrski <77369211+tygrski@users.noreply.github.com>
2023-01-31 15:21:43 -0600

    maps

--- starter-data-structures-operators/script.js
+++ starter-data-structures-operators/script.js
@@ -191,6 +191,46 @@ const restaurant = {
   }
 };
 
+// Maps iteration
+
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C',],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try Again']
+]);
+
+console.log(question);
+
+// CONVERT OBJECT TO MAPS, because Objects are not Iterable
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+
+// Quiz app
+console.log(question.get('question'));
+//  Iteration for map
+for(const [key, value] of question){
+  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+};
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));  
+
+//  Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
 ////////////////////////////////////////////////
 //   SETS Data Structure
 
@@ -285,7 +325,7 @@ rest.set(document.querySelector('h1'), 'Heading')
 console.log(rest);
 console.log(rest.size);
 console.log(rest.get(arr));
-
+*/
 
 
 ////////////////////////////////////////////////////////////
